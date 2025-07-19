fa477bf618d13b8a3757e889c0eda12c0e5dcfc9
Afnan Abdul Vasay <afnanabdulvasay234@gmail.com>
2025-07-19 10:16:44 +0530

    Create storage.py

---
+++ code/storage.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+def load_tasks(filename):
+    if not os.path.exists(filename):
+        return []
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+def save_tasks(filename, tasks):
+    with open(filename, 'w') as file:
+        json.dump(tasks, file, indent=4)
